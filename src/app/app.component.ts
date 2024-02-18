35260c6a0c6a8209727be6a47f24f4ca6f07d489
gabriel ninow <gabrielninow@gmail.com>
2024-02-18 16:52:13 -0300

    added card examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./module/components/navbar/navbar.component";
 import { PageHeaderComponent } from "./module/components/page-header/page-header.component";
+import { CardComponent } from "./module/components/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, NavbarComponent, PageHeaderComponent]
+  imports: [CommonModule, RouterOutlet, NavbarComponent, PageHeaderComponent, CardComponent]
 })
 export class AppComponent {
 
