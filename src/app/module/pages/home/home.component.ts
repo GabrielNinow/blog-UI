52b840577e146d4d1e139877d72ed6dd107fa434
gabriel ninow <gabrielninow@gmail.com>
2024-02-19 05:06:30 -0300

    added footer

--- src/app/module/pages/home/home.component.ts
+++ src/app/module/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { PageHeaderComponent } from "../../components/page-header/page-header.component";
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { CardComponent } from "../../components/card/card.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [PageHeaderComponent, NavbarComponent, CardComponent]
+  imports: [PageHeaderComponent, NavbarComponent, CardComponent, FooterComponent]
 })
 export class HomeComponent {
 
